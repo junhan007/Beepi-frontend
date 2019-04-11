2b0c45ede95f0dacc8977e85b0dd769647c926f5
Junhan <junhanpoh-i@crimsonlogic.com>
2019-04-11 17:45:00 +0800

    new layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { IntroComponent } from './intro/intro.component';
+import { MatButtonModule } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [IntroComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
